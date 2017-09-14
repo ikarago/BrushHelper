ac6223f51de2df671833ff0cdad3db0326482e39
Ikarago <ikarago@outlook.com>
2017-09-13 18:05:25 -0700

    Added simple About popup

--- BrushHelper/Views/MainPage.xaml.cs
+++ BrushHelper/Views/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -23,6 +24,27 @@ namespace BrushHelper.Views
             FrameNormalBrushPage.Navigate(typeof(Views.NormalBrushPage));
             FrameAcrylicBrushPage.Navigate(typeof(Views.AcrylicBrushPage));
             FrameAcrylicBrushMakerPage.Navigate(typeof(Views.AcrylicBrushCreatorPage));
+        }
+
+        private async void cbtnAbout_Click(object sender, RoutedEventArgs e)
+        {
+            ContentDialog openChangelogDialog = new ContentDialog()
+            {
+                Title = "Brush Helper",
+                Content = "v" + GetVersion() + " by Ikarago",
+                PrimaryButtonText = "Close"
+            };
+
+            ContentDialogResult result = await openChangelogDialog.ShowAsync();
+        }
+
+        public static string GetVersion()
+        {
+            Package package = Package.Current;
+            PackageId packageId = package.Id;
+            PackageVersion version = packageId.Version;
+            string versionNumber = string.Format("{0}.{1}.{2}.{3}", version.Major, version.Minor, version.Build, version.Revision);
+            return versionNumber;
         }
     }
 }
